823de33150eaebc7645ab9c4929b24c5fced3800
Dilnura [FE17] <102959751+Dilnura-d@users.noreply.github.com>
2023-01-13 18:06:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ console.log(num)
 
 let text = "Some text"
 alert(text)
+
+let name = John
+console.log(name)
+
+let surname = Smith
+console.log(surname)
+
+let country = USA
+console.log(country)
+
